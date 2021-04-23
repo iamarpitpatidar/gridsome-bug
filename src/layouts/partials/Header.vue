b35cf48c6dbfef8117102650c24852be38e0ab8a
Arpit Patidar <arpit.ptdr@hotmail.com>
2021-04-23 18:46:08 +0530

    foo

--- src/layouts/partials/Header.vue
+++ src/layouts/partials/Header.vue
@@ -3,6 +3,7 @@
     <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
       <div class="relative flex items-center justify-between h-16">
         <div class="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+          <logo />
         </div>
         <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
           <toggle-theme />
@@ -15,11 +16,13 @@
 
 <script>
 import { mapGetters } from 'vuex'
+import Logo from '~/components/Logo'
 import ToggleTheme from '~/components/ToggleTheme'
 import UserProfile from '~/components/UserProfile'
 
 export default {
   components: {
+    Logo,
     ToggleTheme,
     UserProfile
   },
